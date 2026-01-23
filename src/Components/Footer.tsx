722b468bf47cc7f94ccdf412e294571c62cf4534
PROSPER ABAYOMI <pablocodes3846@gmail.com>
2026-01-23 16:37:04 +0100

    Fix import statement for social media icons in Footer component

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaTiktok, FaWhatsapp } from "react-icons/fa6";
+import { FaTiktok, FaWhatsapp } from "react-icons/fa";
 import { FiFacebook, FiInstagram, FiLinkedin, FiTwitter } from "react-icons/fi";
 
 export default function Footer() {
